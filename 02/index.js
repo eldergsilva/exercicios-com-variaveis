ad9ae06ecfac64385b027c36b16b1693b5d2dbf6
unknown <eldergsilva@gmail.com>
2024-03-15 15:02:26 -0300

    exercicio 02 finalizado

---
+++ 02/index.js
@@ -0,0 +1,6 @@
+let grauEmFahrenheit=70;
+let fahrenheitParaCelsius =((grauEmFahrenheit - 32)  / 9) *5
+  
+console.log(` ${grauEmFahrenheit} em Fahrenheit são  ${fahrenheitParaCelsius.toFixed(2)} em Celsius`);
+
+ 
