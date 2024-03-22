7642ff9477663ea43918f0ee637257a32b9271e3
unknown <eldergsilva@gmail.com>
2024-03-22 15:01:38 -0300

    resolvido ex9

---
+++ 09/index.js
@@ -0,0 +1,7 @@
+const numeroDeLados = 6 ;
+const soma = (numeroDeLados-2) * 180
+
+console.log(`A soma dos angulos internos é ${soma}`);
+
+const ai = soma / numeroDeLados ;
+console.log(`Caso os angulos sejam iguais ,cada um deles vale ${ai}`);
