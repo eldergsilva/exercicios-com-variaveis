98d158a21f0eeb8108a492ccf4d9d35363d483bf
unknown <eldergsilva@gmail.com>
2024-03-22 14:57:15 -0300

    resolvido ex8

---
+++ 08/index.js
@@ -0,0 +1,8 @@
+const r = 3;
+const comprimento = 2 * Math.PI * r;
+
+console.log(`O comprimento do circulo é ${comprimento.toFixed(2)}`);
+
+const area = Math.PI * r **2;
+
+console.log(`A area do circulo é ${area.toFixed(2)}`);
